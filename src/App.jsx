1256929c6b9172b24e2a3b15a434f88e0983376b
Wood-Jack <thewoodrowz@gmail.com>
2021-08-10 13:07:43 -0400

    adding neccsary components for parcel to work on project

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className='container'>
+        <textarea className="create-todo-text">I want to..</textarea>
+        <select className="create-todo-priority">
+          <option value="1"></option>
+        </select>
+
+        <a className="edit-todo"></a>
+
+        <a className="delete-todo"></a>
+
+        <textarea className="update-todo-text"></textarea>
+
+        <select className="update-todo-priority"></select>
+
+        <button className= "update-todo"></button>
 
       </div>
     );
