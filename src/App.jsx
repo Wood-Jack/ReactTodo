57b48201aea34450f3eb19dfebaa66decce2afcf
Wood-Jack <thewoodrowz@gmail.com>
2021-08-12 16:18:46 -0400

    Created two files to add into todo and edit the todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,68 @@
+
 import React, { Component } from 'react';
 
-class App extends Component {
+class App extends React.Component {
 
+  constructor(props){
+
+    super(props);
+
+    this.state ={todo:[{list:undefined}]}
+
+    
+  }
+  
+ 
   render(){
 
     return(
+      
+      <div className='container d-flex justify-content-center mt-5'>
+        
+          <div className= "panel-body " id="pbody1">
+            <div class="label-bar " id="label"><h3>Add New Todo</h3></div>
 
-      <div className='container'>
-        <div class= "grid">
-          <div className= "panel-body left">
-            <div class="label-bar"><h3>To do List</h3></div>
-
-            <textarea className="create-todo-text container"> I want to..</textarea>
+            <p className="pt-4 mb-1">I want too..</p>
+            <textarea className="create-todo-text container"></textarea>
             <ul id ="list">
-    
+              
+              {/* {this.state.todos.map(todo => {
+                return <li> todo </li>
+              }} */}
+              <li class="task">
+                <p class="text"></p>
+                <i class="far fa-trash-alt"></i>
+              </li>
+            </ul> 
 
-            </ul>
-            
-
-            <select className="create-todo-priority">
-              <option value="1">Priority List</option>
-            </select>
-            
+            <p className="pt-4 mb-1">How much priority is this?</p>
+            <select className="create-todo-priority"> 
+              <option value="1">Priority List</option> 
+            </select> 
+            <footer id="bottom">
+            <button  type="button" className= "btn btn-primary btn-success">Add</button>
+            </footer>
           </div>
 
-          <button  type="button" class= "btn btn-primary btn-success">+ Add Button</button>
+          
 
-          <div className= "panel-body container">
+          <div className= "panel-body justify-content-center ml-4 h-25 w-50" id="pbody2">
+            <div class="label-bar" id="label" ><h3>View Todos</h3></div>
+            <p className="pt-4 mb-1">Description</p>
+            <textarea className="update-todo-text container">New updates to do text</textarea>
+            
+            <div className= "container-fluid" id="bttm-bar">
 
-            <a className="edit-todo">Edits todo</a>
-            <a className="delete-todo">Deletes todo</a>
+              <row className= "d-flex justify-content-end">
+              <a className="edit-todo " > <i className="far fa-edit"></i> </a>
+              <a className="delete-todo ml-1" >  <i className="far fa-trash-alt"></i> </a></row>
 
-            <textarea className="update-todo-text">New updates to do text</textarea>
-
-            <select className="update-todo-priority">Update Priority</select>
-
-            <button className= "update-todo">updates todo</button> 
-
+              <p className="pt-4 mb-1">Priority</p> 
+              <select className="update-todo-priority fixed-bottom" >Update Priority</select>
+              <button className= "update-todo">updates todo</button>
+            </div>
 
           </div>
-
-        </div>
       </div>
     );
   }
