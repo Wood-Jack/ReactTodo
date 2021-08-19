ae4f1382cacfdfd9f4e3d9c5811b733898d42f7a
Wood-Jack <thewoodrowz@gmail.com>
2021-08-19 10:01:52 -0400

    added  the handleback to access the child class

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,50 @@ class App extends React.Component{
   constructor(props){
 
     super(props);
-    this.state= {listNote:[], notes: this.props.note , priority: this.props.priority };
-    this.updateListArr = this.updateListArr.bind(this);
+    this.state= {listNote:[], notes: ' ' , priority:'' };
+    this.addNoteArr = this.addNoteArr.bind(this);
+    this.deleteListArr = this.deleteListArr.bind(this);
+
+    this.handleCallBack = this.handleCallBack.bind(this);
+    
      
+  }
+
+  //using this function I gain access from the child then able to go to parent class.
+  handleCallBack =(addTodoData) =>{
+
+    this.setState({notes: addTodoData.notes , priority: addTodoData.priority});
+
   }
   
   //creating a previous state to update the array of notes in the app.
-  updateListArr(){
+  addNoteArr(){
 
     let newListNote ={ listNote: this.state.listNote};
     this.setState({listNote:[...this.state.listNote, newListNote]});
 
-    
   }
-
+  
+  //deletes item in the listed array
   deleteListArr(e){
+
     let filteredArray= this.state.listNote.filter(item => item !== e.target.value);
     this.setState({listNote:filteredArray});
+
+  }
+  
+  //updates the todo list itself
+  updateTodoList(){
+
+    const newTodo = {notes: this.state.notes, priority:this.state.priority   
+    }
+
+    this.setState((prevState) =>{const newToDos = prevState.listNote;
+    newToDos.push(newTodo);
+  return{
+    listNote:newToDos
+  };
+  }, () => console.log(this.state.listNote));
   }
   
 
@@ -35,11 +62,11 @@ class App extends React.Component{
 
       <div className="row justify-content-center ">
 
-        <div className="col-8"> <AddTodo updateListArr = {this.updateListArr}/> </div>
+        <div className="col-8"> <AddTodo addNoteArr = {this.addNoteArr}/> </div>
 
         
-        <TodoList notePr= {this.props.note} priorityPr= {this.props.priority} 
-        updateListArr = {this.updateListArr}/>
+        <TodoList parentCallback={this.handleCallBack}
+        addNoteArr = {this.addNoteArr} updateTodoList ={this.updateTodoList}/>
         
 
       </div>
